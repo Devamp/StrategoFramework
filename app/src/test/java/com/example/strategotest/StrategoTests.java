1a54f939d6e22880184e993a40d40f0f702f4995
Gareth <riceg23@up.edu>
2021-03-29 15:36:16 -0700

    Add StrategoTests class

---
+++ app/src/test/java/com/example/strategotest/StrategoTests.java
@@ -0,0 +1,44 @@
+package com.example.strategotest;
+
+/**
+ * @author Gareth Rice
+ * @version 3/21
+ *
+ * Use this for test
+ *
+ * ToDo:
+ * Add @Test tags to appropriate methods
+ */
+public class StrategoTests {
+
+    public void test_checkGamePlay(){
+
+    }
+
+    public void test_CopyConstructorOfState_Empty(){
+
+    }
+
+    public void test_CopyConstructorOfState_InProgress(){
+
+    }
+
+    public void test_CopyConstructorOfState_Full(){
+
+    }
+
+    public void test_Equals_State_Empty(){
+
+    }
+
+    public void test_Equals_State_InProgress(){
+
+    }
+
+    public void test_Equals_State_Full(){
+
+    }
+
+
+
+}
