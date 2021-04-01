3ff69b69b9b6f7b7723608525b5e014465f5821e
Gareth <riceg23@up.edu>
2021-04-01 02:44:53 -0700

    Start game opens our GUI

--- app/src/main/java/com/example/strategotest/Stratego/Players/HumanPlayer.java
+++ app/src/main/java/com/example/strategotest/Stratego/Players/HumanPlayer.java
@@ -2,14 +2,23 @@ package com.example.strategotest.Stratego.Players;
 
 import android.view.View;
 
+import com.example.strategotest.R;
+import com.example.strategotest.Stratego.MainActivity;
+import com.example.strategotest.Stratego.infoMessages.StrategoGameState;
 import com.example.strategotest.game.GameFramework.GameMainActivity;
 import com.example.strategotest.game.GameFramework.infoMessage.GameInfo;
+import com.example.strategotest.game.GameFramework.infoMessage.IllegalMoveInfo;
+import com.example.strategotest.game.GameFramework.infoMessage.NotYourTurnInfo;
 import com.example.strategotest.game.GameFramework.players.GameHumanPlayer;
+import com.example.strategotest.game.GameFramework.utilities.MessageBox;
 
-public class HumanPlayer extends GameHumanPlayer {
+public class HumanPlayer extends GameHumanPlayer implements View.OnClickListener {
 
     private int layoutID;
     private int humanPlayerID;
+
+    private GameMainActivity myActivity;
+
     /**
      * constructor
      *
@@ -30,7 +39,7 @@ public class HumanPlayer extends GameHumanPlayer {
      */
     @Override
     public View getTopView() {
-        return null;
+        return myActivity.findViewById(R.id.mainLayout);
     }
 
     /**
@@ -50,6 +59,17 @@ public class HumanPlayer extends GameHumanPlayer {
      */
     @Override
     public void setAsGui(GameMainActivity activity) {
+        myActivity = activity;
+
+        activity.setContentView(R.layout.activity_main);
+    }
+
+    public void turnMsg(String msg){
+        MessageBox.popUpMessage(msg, myActivity);
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
