8b2d10b407c2ab0504a5cfeff593952ca5f11bbe
Caden <deutsche23@up.edu>
2021-04-16 11:23:43 -0700

    New helper class

---
+++ app/src/main/java/com/example/strategotest/Stratego/SmartHelper.java
@@ -0,0 +1,15 @@
+package com.example.strategotest.Stratego;
+
+public class SmartHelper {
+    private int row;
+   private  int col;
+    private int trow;
+    private int tcol;
+    public SmartHelper(int r,int c,int tr, int tc){
+        row = r;
+        col = c;
+        trow = tr;
+        tcol = tr;
+    }
+
+}
